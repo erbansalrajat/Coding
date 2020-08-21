73254a3ae26b12a2038b5b24c8456bf0f10be1eb
erbansalrajat <68500143+erbansalrajat@users.noreply.github.com>
2020-08-21 12:18:51 +0530

    Update Insertion_Sort.cpp

--- Sorting/Insertion_Sort.cpp
+++ Sorting/Insertion_Sort.cpp
@@ -19,7 +19,7 @@ int main()
 {
 	int array[]={ 20,10,30,35,25,14,40,12,28 };
 	int size=sizeof(array)/sizeof(array[0]);
-	cout<<size<<endl;
+	// cout<<size<<endl;
 	insertionsort(array,size);
 	for(int i=0;i<size;i++)
 	{
